1de070f24c3f60855e262826e939852596da49a5
Caspento <Mrcaspento@gmail.com>
2020-12-09 16:12:05 -0500

    adding somemore markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,24 @@ function generateMarkdown(userQuestions) {
   return `
   # ${userQuestions.title}
   ![badge](https://img.shields.io/static/v1?label=License&message=${userQuestions.license}&color=success)
-  
+========================================================================================================
+
+## **Table of contents**
+* [Description]()
+* [Installation]()
+* [Usage]()
+* [License]()
+* [Contributors]()
+* [Contact]()
+
+## Installation
+${userQuestions.instructions}
+
+## Usage
+${userQuestions.usage} 
+--------------------------------------------------------------------------------------------------------
+ We can test for:
+ ${userQuestions.test}
 `;
 }
 
